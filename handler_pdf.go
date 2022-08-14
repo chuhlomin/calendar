ecc2733d7f98f21461327d2a14a4e565fe385140
Konstantin Chukhlomin <mail@chuhlomin.com>
2022-08-14 19:12:11 -0400

    feat: draw patterns in PDF

--- handler_pdf.go
+++ handler_pdf.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -41,9 +42,70 @@ func createPDF(writer io.Writer, size, orientation, pattern string) error {
 	pdf := gofpdf.New(orientation, "mm", size, "")
 	pdf.SetAutoPageBreak(false, 0)
 
-	err := pdf.Output(writer)
+	pdf.AddPage()
+	pdf.SetDrawColor(0, 0, 0)
+	pdf.SetFillColor(255, 255, 255)
+
+	err := drawPattern(pdf, pattern)
 	if err != nil {
 		return err
+	}
+
+	err = pdf.Output(writer)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func drawPattern(pdf gofpdf.Pdf, pattern string) error {
+	w, h := pdf.GetPageSize()
+	patternSize := 10.0
+
+	switch pattern {
+	case "rect":
+		w, h := pdf.GetPageSize()
+		patternSize := 10.0
+		for x := 0.0; x < w; x += patternSize {
+			for y := 0.0; y < h; y += patternSize {
+				pdf.Rect(x, y, patternSize, patternSize, "D")
+			}
+		}
+	case "dot":
+		pdf.SetFillColor(0, 0, 0)
+		for x := 0.0; x < w; x += patternSize {
+			for y := 0.0; y < h; y += patternSize {
+				pdf.Circle(x, y, 0.2, "F")
+			}
+		}
+	case "diamond":
+		for x := 0.0; x < w; x += patternSize {
+			for y := 0.0; y < h; y += patternSize {
+				pdf.MoveTo(x+patternSize/2, y)
+				pdf.LineTo(x, y+patternSize/2)
+				pdf.LineTo(x+patternSize/2, y+patternSize)
+				pdf.LineTo(x+patternSize, y+patternSize/2)
+				pdf.LineTo(x+patternSize/2, y)
+				pdf.ClosePath()
+				pdf.DrawPath("D")
+			}
+		}
+	case "rhombus":
+		patternHeight := patternSize * 0.6
+		for x := 0.0; x < w; x += patternSize {
+			for y := 0.0; y < h; y += patternHeight {
+				pdf.MoveTo(x+patternSize/2, y)
+				pdf.LineTo(x, y+patternHeight/2)
+				pdf.LineTo(x+patternSize/2, y+patternHeight)
+				pdf.LineTo(x+patternSize, y+patternHeight/2)
+				pdf.LineTo(x+patternSize/2, y)
+				pdf.ClosePath()
+				pdf.DrawPath("D")
+			}
+		}
+	default:
+		return fmt.Errorf("unknown pattern: %s", pattern)
 	}
 
 	return nil
